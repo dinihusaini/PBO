adf88f589ffb58b313b1e0da0b9798d5ab6bbd4a
dinihusaini <dinihusaini2312@gmail.com>
2024-10-24 20:58:39 +0700

    generik.js

--- 08/generik.js
+++ 08/generik.js
@@ -1,4 +1,3 @@
-// Fungsi generik menggunakan pola
 function getElementAtIndex(array, index) {
     if (index >= 0 && index < array.length) {
         return array[index];
@@ -10,6 +9,6 @@ let angka = [23, 12, 04];
 let kata = ["dua", "dua belas", "empat"];
 let tahun = [2002, 2003, 2004];
 
-console.log(getElementAtIndex(angka, 0)); // Output: 20
-console.log(getElementAtIndex(kata, 1)); // Output: "satu"
+console.log(getElementAtIndex(angka, 0)); 
+console.log(getElementAtIndex(kata, 1)); 
 console.log(getElementAtIndex(tahun, 2));
